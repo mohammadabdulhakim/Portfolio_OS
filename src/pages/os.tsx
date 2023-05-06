8e044c2b398fabcc775ab2b4857020b10202f4a7
duxpixels <duxwork0@gmail.com>
2023-05-06 11:05:18 +0300

    avoidRightClick

--- src/pages/os.tsx
+++ src/pages/os.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 import MainParticles from '@/components/particles/MainParticles';
 import { OsContainer, Startup } from '@/components/os'
+import Head from 'next/head';
 
 const os = () => {
     const [waitStartup, setWaitStartup] = useState(true);
@@ -14,11 +15,14 @@ const os = () => {
 
   return (
     <div className="select-none">
+       <Head>
+        <title>Mohammad | OS</title>
+      </Head>
       {waitStartup ? (
-        <>
+        <div onContextMenu={(e)=>e.preventDefault()}>
           <Startup />
           <MainParticles />
-        </>
+        </div>
       ) : (
         <OsContainer />
       )}
