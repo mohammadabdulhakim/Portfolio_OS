5900bdff1327952911706171cba1720f8fdb6efd
duxpixels <duxwork0@gmail.com>
2023-05-27 10:27:36 +0300

    add portfolio components

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -2,31 +2,38 @@
 import Head from "next/head";
 import { animated, useSpring } from "@react-spring/web";
 import Link from "next/link";
+import { CgScrollV } from "react-icons/cg"
 
-import {CgScrollV} from "react-icons/cg"
+import Skills from "@/components/portfolio/Skills";
+import Projects from "@/components/portfolio/Projects";
+import Articles from "@/components/portfolio/Articles";
+import Resume from "@/components/portfolio/Resume";
+import Contact from "@/components/portfolio/Contact";
+
 
 const portfolioNavLinks = [
   {
-    title:"Skills",
-    link:"#skills"
+    title: "Skills",
+    link: "#skills"
   },
   {
-    title:"Projects",
-    link:"#projects"
+    title: "Projects",
+    link: "#projects"
   },
   {
-    title:"Articles",
-    link:"#articles"
+    title: "Articles",
+    link: "#articles"
   },
   {
-    title:"Resume",
-    link:"#resume"
+    title: "Resume",
+    link: "#resume"
   },
   {
-    title:"Contact",
-    link:"#contact"
+    title: "Contact",
+    link: "#contact"
   },
 ]
+
 const PortfolioPage = () => {
   const propsConfig = { mass: 5, tension: 2000, friction: 200 };
 
@@ -39,9 +46,9 @@ const PortfolioPage = () => {
 
   const navProps = useSpring({
     ...propsConfig,
-    delay:500,
+    delay: 500,
     from: { opacity: 0, width: "0" },
-    to: { opacity: 1, width: "400px " },
+    to: { opacity: 1, width: "500px " },
   });
 
   const justProps = useSpring({
@@ -58,11 +65,11 @@ const PortfolioPage = () => {
   });
 
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 text-slate-900 overflow-x-hidden">
       <Head>
         <title>Mohammad | Portfolio</title>
       </Head>
-      <div className="flex items-center justify-evenly h-screen w-screen flex-col">
+      <div className="flex items-center justify-center h-screen w-screen flex-col gap-10">
         <div className="flex items-center flex-col justify-center gap-6">
           <animated.h1
             className={"blue_gradient text_head"}
@@ -71,26 +78,47 @@ const PortfolioPage = () => {
             Start With
           </animated.h1>
           <animated.div style={navProps} className={`p-3 bg-white drop-shadow-md gap-5 flex items-center justify-center px-8`}>
-            {portfolioNavLinks.map((nLink)=>(
-            <Link key={nLink.link} href={`${nLink.link}`} className="text-blue-900 opacity-80 hover:opacity-100 transition-all font-medium">
-              {nLink.title}
-            </Link>
+            {portfolioNavLinks.map((nLink) => (
+              <Link key={nLink.link} href={`${nLink.link}`} className="text-blue-900 opacity-80 hover:opacity-100 transition-all font-medium">
+                {nLink.title}
+              </Link>
             ))}
           </animated.div>
         </div>
-      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="flex flex-col items-center justify-center gap-2">
           <animated.h1 style={justProps} className="text_head orange_gradient">
             Or you can just
           </animated.h1>
           <animated.div style={scrollProps} className="text-slate-700 text-3xl flex items-center justify-center gap-2">
             <span>
-            Scroll down
-            </span> 
+              Scroll down
+            </span>
             <CgScrollV className="bg-white p-1.5 rounded-2xl text-4xl drop-shadow-md" />
           </animated.div>
-      </div>
+        </div>
       </div>
 
+      <div>
+        <div id={`skills`} className="w-screen h-screen">
+          <Skills />
+        </div>
+
+        <div id={`projects`} className="w-screen h-screen">
+          <Projects />
+        </div>
+
+        <div id={`articles`} className="w-screen h-screen">
+          <Articles />
+        </div>
+
+        <div id={`resume`} className="w-screen h-screen">
+          <Resume />
+        </div>
+
+        <div id={`contact`} className="w-screen h-screen">
+          <Contact />
+        </div>
+      </div>
     </div>
   )
 }
