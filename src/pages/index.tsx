e70b3ce16c62b5de8ff93562e96d9c612b404967
duxpixels <duxwork0@gmail.com>
2023-06-21 14:12:39 +0300

    Update my cv

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,22 +27,22 @@ const index = () => {
       <div className="flex flex-col items-center justify-center gap-4 md:w-[60%] w-full">
         <div className="relative mb-8">
           <div className="links-circle w-full h-full rounded-full block absolute z-0 text-neutral-700">
-            <animated.div style={opacityAnimation(1500)}>
+            <animated.div style={opacityAnimation(1000)}>
               <Link href={socialLinks.devToLink} target="_blank" className="hover:text-neutral-900 absolute top-0 left-0 -translate-x-[50%] -translate-y-[50%]">
                 <FaDev size={50} />
               </Link>
             </animated.div>
-            <animated.div style={opacityAnimation(2500)}>
+            <animated.div style={opacityAnimation(1600)}>
               <Link href={socialLinks.githubLink} target="_blank" className="hover:text-neutral-900 absolute top-0 right-0 translate-x-[50%] -translate-y-[50%]">
                 <FaGithubSquare size={50} />
               </Link>
             </animated.div>
-            <animated.div style={opacityAnimation(2000)}>
+            <animated.div style={opacityAnimation(1300)}>
               <Link href={socialLinks.linkedinLink} target="_blank" className="hover:text-neutral-900 absolute bottom-0 left-0 -translate-x-[50%] translate-y-[50%]">
                 <FaLinkedin size={50} />
               </Link>
             </animated.div>
-            <animated.div style={opacityAnimation(3000)}>
+            <animated.div style={opacityAnimation(1900)}>
               <Link href={"/portfolio/contact"} target="_blank" className="hover:text-neutral-900 absolute bottom-0 right-0 translate-x-[50%] translate-y-[50%]" >
                 <MdEmail size={50} />
               </Link>
@@ -53,18 +53,18 @@ const index = () => {
           </animated.div>
         </div>
         <div className="h-fit flex  justify-center flex-col">
-          <animated.div style={textAnimation(3600, "40px")} className="text-2xl font-semibold text-start md:ml-20" >
+          <animated.div style={textAnimation(2400, "40px")} className="text-2xl font-semibold text-start md:ml-20" >
             Hello There,
           </animated.div>
-          <animated.div style={textAnimation(4000, "40px")} className="text-3xl font-semibold text-center" >
+          <animated.div style={textAnimation(2800, "40px")} className="text-3xl font-semibold text-center" >
             I&apos;m Mohammad AbdulHakim 
             <span className="text-[15px] font-normal">
               {" "}Frontend web developer
             </span>
           </animated.div>
-          <animated.div style={textAnimation(4300, "28px")} className="text-lg sm:text-xl text-gray-800 text-center md:mt-0 mt-14" >
+          <animated.div style={textAnimation(3300, "28px")} className="text-lg sm:text-xl text-gray-800 text-center md:mt-0 mt-14" >
             This is just a welcome page. You can visit{" "}
-            <Link className="text-blue-600" target="_blank" href={`/portfolio`}>
+            <Link className="text-blue-600" href={`/portfolio`}>
               my portfolio{" "}
             </Link>
             to view the following sections:{" "}
@@ -92,11 +92,11 @@ const index = () => {
               interactive PC
             </Link>
             {" "}and surf its{" "}
-            <Link className="text-blue-600" target="_blank" href={`/pc/os`}>
+            <Link className="text-blue-600" href={`/pc/os`}>
               operating system
             </Link>
             {" "}to get a glimpse of my technical abilities. If you have any questions or comments, please feel free to contact me at{" "}
-            <Link className="text-blue-600" target="_blank" href={"mailto:contact@abomisr.com"}>
+            <Link className="text-blue-600" href={"mailto:contact@abomisr.com"}>
               contact@abomisr.com
             </Link>
             .
